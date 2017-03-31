36532fce06c92140323ac3790c0cb92c0ff7f631
ankurpapneja <attheencores@gmail.com>
2017-03-31 03:44:56 +0000

    initiate OCR on images from server

--- scribeAPI/image_handling.py
+++ scribeAPI/image_handling.py
@@ -5,7 +5,7 @@
 # Initiate OCR
 
 import os
-import subprocess
+import requests, subprocess
 import time
 
 start_time = time.time()
@@ -16,6 +16,8 @@ minutes_to_wait = 24 * 60
 image_names = []
 image_urls = []
 
+OCR_POST_URL = "http://ec2-54-173-153-28.compute-1.amazonaws.com:8080/fileAPI/imageURL"
+    
 while(True):
 
     ###############################################
@@ -42,7 +44,7 @@ while(True):
 
     ###############################################
 
-    # grab all new images from server not found in Scribe
+    # grab all new images from server not found in Scribe and initiate OCR
     base_url = "http://ec2-54-173-153-28.compute-1.amazonaws.com/test_images/"
     with open("project/cc-ing/subjects/group_only_one_group.csv", "a") as scribeFile:
         for imageFile in os.listdir("/var/www/html/test_images"):
@@ -50,26 +52,12 @@ while(True):
                 url = base_url + imageFile
                 if url not in file_contents:
                     to_write = url + ",100,100\n"
-                    scribeFile.write(to_write)
+#                    scribeFile.write(to_write)
                     print to_write
-                    
+
+                    # initiate OCR
+                    r = requests.post(OCR_POST_URL, data={'imageURL': url})
+                    print r.json()
+
 # end while
 
-########################################################
-
-def do_OCR(url):
-    print "wget'ing from Box img", url
-    subprocess.check_call(["wget", url])
-    subprocess.check_call(["mv", "thumb_94.jpg?shared_name=kanug6fkviyncr9onmjkaq4xew3x4zek", "to_be_ocrd.jpg"])
-
-    params = {'file': "@to_be_ocrd.jpg"}
-    POST_URL = "http://ec2-54-173-153-28.compute-1.amazonaws.com:8080/fileAPI"
-
-    print "POST'ing to ocr img:", url
-    subprocess.check_output(["curl", "-X POST", POST_URL, "-F file=@to_be_ocrd.jpg"])
-                             
-    #r = requests.post(POST_URL, data=params)
-    #print str(r.text)
-
-    print "Done OCR'ing URL", url
-    subprocess.call(["rm", "to_be_ocrd.jpg"])
