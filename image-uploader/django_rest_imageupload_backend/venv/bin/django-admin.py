3c8fb957d301e5780d73525a96c64a10ada99fd2
Jeffrey Qiu <jeffreyyqiu@gmail.com>
2017-05-26 16:36:43 -0700

    Reset directory

--- image-uploader/django_rest_imageupload_backend/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/jeffreyqiu/Desktop/BuildUCLA/CC-ing/django-rest-imageupload/django_rest_imageupload_backend/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- image-uploader/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/jeffreyqiu/Desktop/BuildUCLA/CC-ing/django-rest-imageupload/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
