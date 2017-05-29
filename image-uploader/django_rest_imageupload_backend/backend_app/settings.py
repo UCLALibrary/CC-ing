8e4936135cafbb4ecb26a1523fdd0c1e8c0208a4
Jeffrey Qiu <jeffreyyqiu@gmail.com>
2017-05-28 18:17:57 -0700

    fixed broken thumbnail images

--- image-uploader/django_rest_imageupload_backend/backend_app/settings.py
+++ image-uploader/django_rest_imageupload_backend/backend_app/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'sample_images')
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'uploaded_media')
 
 
 INSTALLED_APPS += ('lockdown', )
--- image-uploader/django_rest_imageupload_backend/backend_app/urls.py
+++ image-uploader/django_rest_imageupload_backend/backend_app/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # anything going to /api gets redirected to the django rest API stuff
     url(r'^api/', include('imageupload_rest.urls', namespace='api')),
-] 
-# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # we are serving static and media files here at the moment - if we deploy this app to a server, we do necessarily want this
 
