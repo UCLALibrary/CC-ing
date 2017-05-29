dcba501a889825514a1e6cd3a0bacb716c69dc5a
Darthpwner <matthewallenlin@gmail.com>
2017-05-28 20:46:03 -0700

    Able to get ID

--- json_to_xml.py
+++ json_to_xml.py
@@ -11,14 +11,33 @@ obj = json.loads(content)
 # URL of image
 # Most popular image for author
 # Most popular image for title
+dataDict = []
+
+#  To handle dictionary of list of dictionary
 for key, value in obj.items():
-	print("KEY: " + str(key))
-	print("VALUES: " + str(value))
-	print("\n")
+	# data is the only meaningful key
+	if(key == "data"):
+		print("KEY: " + str(key))
+		print("VALUES: " + str(value))
+		print("\n\n\n\n\n")
+
+		# To handle list of dictionary
+		count = 0
+		for i in value:
+			print("i: " + str(i))
+
+			print("ID of the image: " + str(value[count]["subject_set_id"]))
+			print("URL of the image: " + str(value[count][]))
+
+
+			count += 1
+			print("\n\n\n\n\n")
+
+
 
 
 # print(obj)
-print("\n\n\n\n\n\n\n\n\n\n")
+# print("\n\n\n\n\n\n\n\n\n\n")
 
 #xml = dicttoxml.dicttoxml(obj)
 #print(xml)
