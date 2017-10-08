aad898aad8251588a46aac7e45a810cdffdc8855
DaLarm Han <dalarm@g.ucla.edu>
2017-10-07 21:25:32 -0700

    Took away unnecessary code.

--- image-uploader/django_rest_imageupload_backend/imageupload_frontend/urls.py
+++ image-uploader/django_rest_imageupload_backend/imageupload_frontend/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url, include
-from django.views.generic.base import RedirectView
 from . import views
 # Redirect any request that goes into here to static/index.html
 urlpatterns = [
